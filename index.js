a5fa6491b6726f438221b16bd8f0bb7d8f395d59
Izabela Pereira <92803206+izgpbela@users.noreply.github.com>
2023-03-21 15:18:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+<a id="desafio3"></a>
+## Aula 3 -> Implementar outras versões da estrutura de repetição que fizemos com for, como por exemplo com foreach ou while para exibir a lista de filmes na tela
+
+Para este desafio, vou mostrar o código feito com for, foreach e while.
+
+#### For
+
+```
 var listaFilmes = [
   "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg",
   "https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
@@ -16,3 +24,41 @@ var listaFilmes = [
 for (var i = 0; i < listaFilmes.length; i++) {
   document.write("<img src=" + listaFilmes[i] + ">");
 }
+```
+
+#### Foreach
+
+O método `forEach()` executa uma dada função em cada elemento de um array. Caso queira ver mais exemplos, [segue o link](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach).
+
+```
+var listaFilmes = [
+  "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg",
+  "https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
+  "https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg",
+  "https://upload.wikimedia.org/wikipedia/pt/7/76/10_Things_I_Hate_About_You.jpg",
+  "https://br.web.img2.acsta.net/medias/nmedia/18/91/08/82/20128877.JPG"
+];
+
+listaFilmes.forEach(function(filme) {
+  document.write("<img src=" + filme + ">");
+});
+
+```
+
+#### while
+
+```
+var listaFilmes = [
+"https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg",
+"https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
+"https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg",
+"https://upload.wikimedia.org/wikipedia/pt/7/76/10_Things_I_Hate_About_You.jpg",
+"https://br.web.img2.acsta.net/medias/nmedia/18/91/08/82/20128877.JPG"
+];
+
+var i = 0;
+while (i < listaFilmes.length) {
+document.write("<img src=" + listaFilmes[i] + ">");
+i++;
+}
+```
